a4fe521abd0008fc1ecf7d5c85c12700e6e9b098
saulius334 <105633620+saulius334@users.noreply.github.com>
2022-09-05 11:58:14 +0300

    1st day

---
+++ App/DB/Json.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\DB;
+
+class Json implements DataBase {
+
+    private function __construct() {
+        if (!file_exists(DIR . ))
+    }
+
+
+
+
+
+
+
+
+
+
+    function create(array $userData) : void {
+
+    }
+ 
+    function update(int $userId, array $userData) : void {
+
+    }
+ 
+    function delete(int $userId) : void {
+
+    }
+ 
+    function show(int $userId) : array {
+
+    }
+    
+    function showAll() : array {
+
+    }
+}
