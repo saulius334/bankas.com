6b6543d819cf7212360d16f8a7aa7b47305dfcf0
saulius334 <105633620+saulius334@users.noreply.github.com>
2022-09-06 11:24:20 +0300

    nav + validation + login + register

--- Resources/view/nav.php
+++ Resources/view/nav.php
@@ -1,21 +1,29 @@
+<?php
+if (App\Middleware\Auth::isLogged()) : ?>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-12">
             <ul class="nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="<?= URL ?>">HOME</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="<?= URL ?>list">Users</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="<?= URL ?>user/create">New User</a>
-                </li>
-                <li class="nav-item">
-                    <div class="user-nav">
-                    <div class="name"><?= $_SESSION['player']['name'] ?></div>
-                    </div>
-                </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?= URL ?>">HOME</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?= URL ?>list">Users</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?= URL ?>user/create">New User</a>
+                    </li>
+                    <li class="nav-item">
+                        <div class="user-nav">
+                        <div class="name"><?= $_SESSION['player']['name'] ?></div>
+                        </div>
+                    </li>
+                    <?php else : ?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?= URL ?>login">Login</a>
+                        </li>
+                        
+                            <?php endif ?>
             </ul>
         </div>
     </div>
