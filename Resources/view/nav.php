2dbe6ae2db498f354343a36ef0c6686e76a9a0f6
saulius334 <105633620+saulius334@users.noreply.github.com>
2022-09-12 09:23:51 +0300

    scss

--- Resources/view/nav.php
+++ Resources/view/nav.php
@@ -13,9 +13,9 @@ if (App\Middleware\Auth::isLogged()) : ?>
                     <li class="nav-item">
                         <a class="nav-link" href="<?= URL ?>user/create">New User</a>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item last">
                     <div class="user-nav">
-                    <div class="name">Logged in as <?= $_SESSION['player']['name'] ?></div>
+                    <div class="name">Logged in as <span class="namebold"><?= $_SESSION['player']['name'] ?></span></div>
                     <form action="<?= URL ?>logout" method="POST">
                         <button type="submit" class="btn btn-outline-danger m-2">Logout</button>
                     </form>
